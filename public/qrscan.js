19318f2f0c93abd47be16b768580644253943353
masahiro <m.xpe602@gmail.com>
2021-11-02 20:00:12 +0900

    4nd commit

--- public/qrscan.js
+++ public/qrscan.js
@@ -13,7 +13,7 @@ function initializeLiff(defaultLiffId) {
           const stringifiedResult = result.value;
           liff.sendMessages([{
               'type': 'text',
-              'text': stringifiedResult //aaa
+              'text': stringifiedResult
           }]).then(() => {
               liff.closeWindow();
           }).catch((error) => {
